689fe29f49cc523d82b9c8d1a234ebb75cbcff0f
Athul Antony N.P <athulantonynp@gmail.com>
2016-11-07 12:04:56 +0530

    Created Sign Up Activity
    Using Firebase

--- PSCTroll/app/src/main/java/com/asamz/psctroll/SignUpActivity.java
+++ PSCTroll/app/src/main/java/com/asamz/psctroll/SignUpActivity.java
@@ -2,15 +2,26 @@ package com.asamz.psctroll;
 
 import android.app.ActionBar;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.graphics.PorterDuff;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.io.UnsupportedEncodingException;
 import java.util.Properties;
@@ -25,6 +36,10 @@ import javax.mail.Transport;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import pl.droidsonroids.gif.GifTextView;
 
 import static android.provider.ContactsContract.Intents.Insert.EMAIL;
 
@@ -32,6 +47,15 @@ public class SignUpActivity extends AppCompatActivity {
    private ImageView ivSignup;
    private  EditText etFirstName,etSecondName,etEmail,etPassword;
     private String firstName,secondName,emailId,passWord;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
+    GifTextView gfStatus;
+    Animation animationFadeIn;
+    Animation animationFadeOut;
+    FirebaseUser firebaseUser = null;
+    private FirebaseAuth mAuth;
+    int myphase=0;
+    int i=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +66,12 @@ public class SignUpActivity extends AppCompatActivity {
         etEmail=(EditText)findViewById(R.id.etEmail);
         etPassword=(EditText)findViewById(R.id.etPassword);
         ivSignup=(ImageView)findViewById(R.id.ivSignUp);
+        firebaseAuth = FirebaseAuth.getInstance();
+        gfStatus=(GifTextView)findViewById(R.id.gfSignUp);
+        gfStatus.setVisibility(View.INVISIBLE);
         //setting signup image clicking effect
+         animationFadeIn = AnimationUtils.loadAnimation(this, R.anim.fadein);
+         animationFadeOut = AnimationUtils.loadAnimation(this, R.anim.fadeout);
         ivSignup.setOnTouchListener(new View.OnTouchListener() {
 
             @Override
@@ -82,30 +111,46 @@ public class SignUpActivity extends AppCompatActivity {
             return false;
     }
     public boolean passwordMin(String password){
-        if(password.length()>6){
+        if(password.length()>=6){
             return true;
         }
         else
             return false;
     }
     public void signUpProcedure(View view){
-        if(etFirstName.getText().toString()==null||etSecondName.getText().toString()==null||etEmail.getText().toString()==null||etPassword.getText().toString()==null){
-            Toast.makeText(getApplicationContext(), "Please Fill Appropirate Fields", Toast.LENGTH_SHORT).show();
+        if(isNetworkAvailable()==false){
+            Toast.makeText(getApplicationContext(), "Connect to an active internet connection", Toast.LENGTH_SHORT).show();
+        }
+        else {
+
+            firstName = etFirstName.getText().toString();
+            secondName = etSecondName.getText().toString();
+            emailId = etEmail.getText().toString();
+            passWord = etPassword.getText().toString();
+            if (isValidEmailAddress(emailId) == false) {
+                etEmail.setHint(R.string.emailError);
+            }
+            if (passwordMin(passWord) == false) {
+                etPassword.setHint(R.string.passwordError);
+            }
+
+            if (isValidEmailAddress(emailId) == true && passwordMin(passWord) == true) {
+                ivSignup.startAnimation(animationFadeOut);
+                ivSignup.setVisibility(View.INVISIBLE);
+                if(isEmailAlreadyUsed(emailId)==true){
+                    ivSignup.startAnimation(animationFadeIn);
+                    ivSignup.setVisibility(View.VISIBLE);
+                    Toast.makeText(getApplicationContext(), "Email ID is already in use", Toast.LENGTH_SHORT).show();
+
+                }
+                else {
+
+                    sendMail(emailId, "Hi", "Dear " + firstName + " " + secondName + ", You have successfully completed the registration.\n Your Password is " + passWord + " \n Please Login to app using the same EmailID and Password");
+                }
+            }
 
         }
-        firstName=etFirstName.getText().toString();
-        secondName=etSecondName.getText().toString();
-        emailId=etEmail.getText().toString();
-        passWord=etPassword.getText().toString();
-        if(isValidEmailAddress(emailId)==false){
-            etEmail.setText("Please Enter a valid email ID");
-        }
-        if(passwordMin(passWord)==false){
-            Toast.makeText(getApplicationContext(), "Password should have atleast 6 characters", Toast.LENGTH_SHORT).show();
-        }
-        if(isValidEmailAddress(emailId)==true&&passwordMin(passWord)==true){
-            sendMail(emailId, "Hi", "Dear " + firstName+" "+secondName+ ", You have successfully completed the registration.\n Your Password is "+passWord+" \n Please Login to app using the same EmailID and Password");
-        }
+
 
         }
 
@@ -151,27 +196,107 @@ public class SignUpActivity extends AppCompatActivity {
 
 
     public class SendMailTask extends AsyncTask<Message, Void, Void> {
-        private ProgressDialog progressDialog;
+
 
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            progressDialog = ProgressDialog.show(SignUpActivity.this, "Please wait", "Sending mail", true, false);
+            gfStatus.setVisibility(View.VISIBLE);
+            gfStatus.startAnimation(animationFadeIn);
+
+
         }
 
         @Override
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
-            progressDialog.dismiss();
+
+                if(i==2){
+                    gfStatus.startAnimation(animationFadeOut);
+                    gfStatus.setVisibility(View.INVISIBLE);
+                    ivSignup.startAnimation(animationFadeIn);
+                    ivSignup.setVisibility(View.VISIBLE);
+                    Toast.makeText(getApplicationContext(), "Registration is Unsuccessfull,Do it Again", Toast.LENGTH_SHORT).show();
+
+                }
+                gfStatus.startAnimation(animationFadeOut);
+                gfStatus.setVisibility(View.INVISIBLE);
+                Toast.makeText(getApplicationContext(), "Registration Successfull", Toast.LENGTH_SHORT).show();
+
+
+
         }
 
         protected Void doInBackground(javax.mail.Message... messages) {
             try {
                 Transport.send(messages[0]);
+                registerUserOnFireBase(emailId,passWord);
+
+
             } catch (MessagingException e) {
-                e.printStackTrace();
+
+               e.printStackTrace();
+
             }
+
             return null;
         }
 }
+    private void registerUserOnFireBase(String email,String password){
+
+
+
+        //creating a new user
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //checking if success
+                        if(task.isSuccessful()){
+                            i=0;
+
+                        }else{
+                            //display some message here
+                            i=2;
+
+
+                        }
+
+                    }
+                });
+
+    }
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+    private boolean isEmailAlreadyUsed(String EmailID) {
+        mAuth = FirebaseAuth.getInstance();
+
+
+
+        mAuth.createUserWithEmailAndPassword(EmailID, "dummypass")
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+                            /////user do not exit so good to initialize firebase user.
+                            firebaseUser = task.getResult().getUser();
+
+                        } else {
+                            if(task.getException().getMessage().equals("The email address is already in use by another account.")) {
+                               myphase=1;
+                            }
+                        }
+                    }
+                });
+        if(myphase==0){
+            return false;
+        }
+        else
+            return true;
+    }
 }
