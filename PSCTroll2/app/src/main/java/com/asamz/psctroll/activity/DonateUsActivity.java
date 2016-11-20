9bf4765f3b8a6ed0ade0a989f70b55fae508d169
Athul Antony N.P <athulantonynp@gmail.com>
2016-11-20 11:44:45 +0530

    Navigation Drawer Menu_Part 1

---
+++ PSCTroll2/app/src/main/java/com/asamz/psctroll/activity/DonateUsActivity.java
@@ -0,0 +1,15 @@
+package com.asamz.psctroll.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.asamz.psctroll.R;
+
+public class DonateUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donate_us);
+    }
+}
